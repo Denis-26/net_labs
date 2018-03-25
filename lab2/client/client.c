8581f7005debdc34d985c8d8d889fdad8c0badab
Denis-26 <kartavenkodenis0@gmail.com>
2018-03-26 00:06:18 +0300

    lab2

---
+++ lab2/client/client.c
@@ -0,0 +1,64 @@
+#include <stdio.h> //printf
+#include <string.h> //memse
+#include <stdlib.h> //exit(0);
+#include <unistd.h> //close()
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define SERVER "192.168.0.255"
+#define BUFLEN 512
+#define PORT 4000
+
+void die(char *s){
+    perror(s);
+    exit(1);
+}
+
+int inet_aton(const char *cp, struct in_addr *inp);
+
+int main(int argc, char *argv[]){
+    struct sockaddr_in si_other;
+    int s;
+    socklen_t slen = sizeof(si_other);
+    char message[BUFLEN];
+
+    if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1){
+        die("socket");
+    }
+
+    int broadcastPermission = 1;
+    if (setsockopt(s, SOL_SOCKET, SO_BROADCAST, (void *) &broadcastPermission,
+          sizeof(broadcastPermission)) < 0)
+        printf("%s\n", "setsockopt() failed");
+
+    memset((char *) &si_other, 0, sizeof(si_other));
+    si_other.sin_family = AF_INET;
+    si_other.sin_port = htons(PORT);
+
+    if (inet_aton(SERVER , &si_other.sin_addr) == 0){
+        fprintf(stderr, "inet_aton() failed\n");
+        exit(1);
+    }
+
+    while(1){
+        printf("Enter word : ");
+        scanf("%s", message);
+
+        if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &si_other, slen)==-1){
+            die("sendto()");
+        }
+
+        int server_answer = 0;
+        if (recvfrom(s, &server_answer, 1, 0, (struct sockaddr *) &si_other, &slen) == -1){
+            die("recvfrom()");
+        }
+
+        if (server_answer){
+          printf("%s is %s\n", message, "polindrom");
+        } else {
+          printf("%s is not %s\n", message, "polindrom");
+        }
+    }
+    close(s);
+    return 0;
+}
---
+++ lab2/server/server.c
@@ -0,0 +1,70 @@
+#include<stdio.h> //printf
+#include<string.h> //memset
+#include<stdlib.h> //exit(0);
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include <unistd.h> //close()
+#include <string.h>
+
+#define BUFLEN 512
+
+void die(char *s){
+    perror(s);
+    exit(1);
+}
+
+int ispolindrom(char *str){
+  int str_len = strlen(str);
+  for (int i = 0; i < str_len; ++i){
+    if (str[i] != str[str_len-1]){
+      return 0;
+    }
+    str_len--;
+  }
+  return 1;
+}
+
+int main(int argc, char *argv[]){
+    struct sockaddr_in si_me, si_other;
+
+    int s, recv_len;
+    socklen_t slen = sizeof(si_other);
+    char buf[BUFLEN];
+
+    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1){
+        die("socket");
+    }
+    memset((char *) &si_me, 0, sizeof(si_me));
+
+    si_me.sin_family = AF_INET;
+    si_me.sin_port = htons(atoi(argv[1]));
+    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1){
+        die("bind");
+    }
+
+    while(1){
+        printf("Waiting for data...");
+        fflush(stdout);
+        memset(buf,'\0', BUFLEN);
+
+        if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1){
+            die("recvfrom()");
+        }
+
+
+        printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+        printf("Data: %s\n" , buf);
+
+        int polindrom = ispolindrom(buf);
+
+        if (sendto(s, &polindrom, 1, 0, (struct sockaddr*) &si_other, slen) == -1)
+        {
+            die("sendto()");
+        }
+    }
+
+    close(s);
+    return 0;
+}
